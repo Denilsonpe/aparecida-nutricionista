583d6c4d2df19a73a81e29b31413cf077375dd61
Denilson Petronio de almeida <72873022+Denilsonpe@users.noreply.github.com>
2020-10-14 11:41:01 -0300

    Add files via upload
    arquivos do projeto do curso alura oracle-one

---
+++ aparecida-nutricionista/js/bucar.js
@@ -0,0 +1,47 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function(){
+    console.log("buscando pacientes.....");
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function(){
+        var erroAjax = document.querySelector("#erro-ajax");
+        
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }        
+    });        
+    
+    xhr.send();
+});
+
+
+/* if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
+        
+        validaçao de erros*/
+
+
+
+/*
+  https://api-pacientes.herokuapp.com/pacientes
+*/
